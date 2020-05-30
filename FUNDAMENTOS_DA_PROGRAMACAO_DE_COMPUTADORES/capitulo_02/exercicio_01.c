b772e9c55255ae95822e2a68d8c354d466fff908
WandersonGomes <dersom100@gmail.com>
2020-05-30 03:59:51 -0300

    Exercicio 01

---
+++ FUNDAMENTOS_DA_PROGRAMACAO_DE_COMPUTADORES/capitulo_02/exercicio_01.c
@@ -0,0 +1,23 @@
+/*
+Enunciado:
+Faca um programa que receba dois numeros, calcule e mostre a subtracao do primeiro
+numero pelo segundo. 
+*/
+#include <stdio.h>
+
+int main() {
+    int numero1 = 0, numero2 = 0;
+    int resultado = 0;
+
+    printf("Informe o primeiro numero inteiro: ");
+    scanf("%d", &numero1);
+
+    printf("Informe um segundo numero inteiro: ");
+    scanf("%d ", &numero2);
+
+    resultado = numero1 - numero2;
+
+    printf("O resultado de %d - %d eh %d.\n", numero1, numero2, resultado);
+
+    return 0;
+}
