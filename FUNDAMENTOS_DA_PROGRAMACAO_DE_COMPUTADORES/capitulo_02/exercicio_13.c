63a22d546ecf4fc7fa93280b42dbb9c58de7ea3c
WandersonGomes <dersom100@gmail.com>
2020-05-30 15:42:02 -0300

    Exercicio 13

--- FUNDAMENTOS_DA_PROGRAMACAO_DE_COMPUTADORES/capitulo_02/exercicio_13.c
+++ FUNDAMENTOS_DA_PROGRAMACAO_DE_COMPUTADORES/capitulo_02/exercicio_13.c
@@ -4,14 +4,21 @@ Faca um programa que calcule e mostre a tabuada de um numero digitado pelo usuar
 #include <stdio.h>
 
 int main() {
-    int numero = 0, i = 0;
+    int numero = 0;
 
     printf("Informe um numero para gera a tabuada: ");
     scanf("%d", &numero);
 
-    for (i = 1; i <= 10; i++) {
-        printf("%d x %d = %d\n", numero, i, (numero * i));
-    }
+    printf("%d x %d = %d\n", numero, 1, (numero * 1));
+    printf("%d x %d = %d\n", numero, 2, (numero * 2));
+    printf("%d x %d = %d\n", numero, 3, (numero * 3));
+    printf("%d x %d = %d\n", numero, 4, (numero * 4));
+    printf("%d x %d = %d\n", numero, 5, (numero * 5));
+    printf("%d x %d = %d\n", numero, 6, (numero * 6));
+    printf("%d x %d = %d\n", numero, 7, (numero * 7));
+    printf("%d x %d = %d\n", numero, 8, (numero * 8));
+    printf("%d x %d = %d\n", numero, 9, (numero * 9));
+    printf("%d x %d = %d\n", numero, 10, (numero * 10));
 
     return 0;
 }
