ac4c2db4b307b11d9c29a5d5420d4ac7dd8283d0
WandersonGomes <dersom100@gmail.com>
2020-05-30 02:29:15 -0300

    Insert Sort

--- ALGORITMOS_TEORIA_E_PRATICA/capitulo_02/insertion_sort.c
+++ ALGORITMOS_TEORIA_E_PRATICA/capitulo_02/insertion_sort.c
@@ -4,22 +4,26 @@
 //https://www.youtube.com/watch?v=ROalU379l3U
 #include <stdio.h>
 
+#define QTD_ITENS_ARRANJO 6
+
 int main() {
+    printf("INSERTION SORT\n");
     int arranjo[] = {5, 2, 4, 6, 1, 3};
     int i = 0, j = 0, chave = 0;
 
     //apresenta vetor antes da ordenacao
-    for (i = 0; i < 6; i++) {
+    printf("VETOR NAO ORDENADO:\n");
+    for (i = 0; i < QTD_ITENS_ARRANJO; i++) {
         printf("%d ", arranjo[i]);
     }
     printf("\n");
 
     //ordena o vetor (arranjo)
-    for (i = 1; i < 6; i++) {
+    for (i = 1; i < QTD_ITENS_ARRANJO; i++) {
         chave = arranjo[i];
         j = i - 1;
         
-        while ((j > 0) && (arranjo[j] > chave)) {
+        while ((j >= 0) && (arranjo[j] > chave)) {
             arranjo[j+1] = arranjo[j];
             j = j - 1;
         }
@@ -28,7 +32,8 @@ int main() {
     }
 
     //apresenta o resultado
-    for (i = 0; i < 6; i++) {
+    printf("VETOR ORDENADO:\n");
+    for (i = 0; i < QTD_ITENS_ARRANJO; i++) {
         printf("%d ", arranjo[i]);
     }
     printf("\n");
